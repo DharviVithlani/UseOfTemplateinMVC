ee723e1e43ab09255ca134a5d44c2c09d0348c7c
Dharvi <tbs.dharvi@gmail.com>
2021-10-22 12:31:11 +0530

    Add Columns Country,State and City and apply searching,sorting into it.

--- UseOfTemplateInMVC/Controllers/LoginV2Controller.cs
+++ UseOfTemplateInMVC/Controllers/LoginV2Controller.cs
@@ -24,23 +24,23 @@ namespace UseOfTemplateInMVC.Controllers
                 var userdetails = Registration.GetUserByUserName(username);
                 if (userdetails == null)
                 {
-                    return Json(new { isValid = false, errorMessage = "UserName is incorrect." }, JsonRequestBehavior.AllowGet);
+                    return Json(new { success = false, errorMessage = "UserName is incorrect." }, JsonRequestBehavior.AllowGet);
                 }
                 else if (password != userdetails.Password)
                 {
-                    return Json(new { isValid = false, errorMessage = "Password is incorrect." }, JsonRequestBehavior.AllowGet);
+                    return Json(new { success = false, errorMessage = "Password is incorrect." }, JsonRequestBehavior.AllowGet);
                 }
                 else
                 {
                     Session["id"] = userdetails.UserId.ToString();
                     Session["name"] = userdetails.FirstName != null ? userdetails.FirstName.ToString() : "Guest";
                     Session["image"] = userdetails.ProfileImage != null ? userdetails.ProfileImage.ToString() : Url.Content(Constants.DefaultUserImage);
-                    return Json(new { isValid = true, errorMessage = "" }, JsonRequestBehavior.AllowGet);
+                    return Json(new { success = true, errorMessage = "" }, JsonRequestBehavior.AllowGet);
                 }
             }
             catch(Exception e)
             {
-                return Json(new { isValid = false, errorMessage = "Something went wrong." }, JsonRequestBehavior.AllowGet);
+                return Json(new { success = false, errorMessage = "Something went wrong." }, JsonRequestBehavior.AllowGet);
             }
         }
         public ActionResult Logout()
