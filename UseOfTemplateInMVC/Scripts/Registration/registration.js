06ade8200f2fdc674844b8cb3d5f7e66120d843c
Dharvi <tbs.dharvi@gmail.com>
2021-10-28 10:25:11 +0530

    Add javascript files.

---
+++ UseOfTemplateInMVC/Scripts/Registration/registration.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    setTimeout(function () {
+        $("#msg").fadeOut();
+    }, 5000);
+
+    $("#CountryId").change(function () {
+        var countryid = $(this).val();
+        $.ajax({
+            type: "GET",
+            url: "/Registration/GetStateList?CountryId=" + countryid,
+            contentType: "html",
+            success: function (response) {
+                $("#StateId").empty();
+                $("#CityId").empty();
+                $("#StateId").append(response);
+                $("#CityId").append('<option value="0">Select City</option>');
+            }
+        });
+    });
+
+    $("#StateId").change(function () {
+        var stateid = $(this).val();
+        $.ajax({
+            type: "GET",
+            url: "/Registration/GetCityList?StateId=" + stateid,
+            contentType: "html",
+            success: function (response) {
+                $("#CityId").empty();
+                $("#CityId").append(response);
+            }
+        });
+    });
+});
---
+++ UseOfTemplateInMVC/Scripts/Upload/uploadImage.js
@@ -0,0 +1,44 @@
+﻿$(function () {
+    bsCustomFileInput.init();
+    //bind event
+    //$('#ProfileImage').on('change', function () {
+    //   uploadmessage();
+    //});
+});
+var isFileValid = false;
+function OnBegin() {
+    var imagename = $("#ProfileImage").val();
+    if (imagename == "") {
+        $('#message').text("Please choose an Image.");
+        return false;
+    }
+    return isFileValid;
+}
+
+function OnSuccess(imagename) {
+    Swal.fire(
+        'Great!',
+        'Your Profile has been updated successfully!',
+        'success'
+    )
+    $("#modal-default").modal("hide");
+    $('#userimage,#userprofilepicture').attr("src", "/Images/ProfileImages/" + imagename);
+}
+
+function uploadImage() {
+    var ext = $("#ProfileImage").val().split('.').pop().toLowerCase();
+    if ($.inArray(ext, ['gif', 'png', 'jpg', 'jpeg']) == -1) {
+        isFileValid = false;
+        $('#message').text("file format is incorrect.");
+    }
+    else {
+        isFileValid = true;
+        $("#message").text("");
+    }
+}
+
+$("#modal-default").on('show.bs.modal', function () {
+    $('#message').text("");
+    $("#ProfileImage").val('');
+    $('#lblImageName').text('Choose File');
+});
