f74592a00886e071e0c03230b54230d36d263598
Dharvi <tbs.dharvi@gmail.com>
2021-11-02 15:01:14 +0530

    Apply hide and show password functionality.

---
+++ UseOfTemplateInMVC/Scripts/ShowHidePassword/showHidePassword.js
@@ -0,0 +1,9 @@
+﻿function showHidePassword(element) {
+    $(element).toggleClass("fa-eye fa-eye-slash");
+    var passInput = $(element).parent().parent().prev();
+    if (passInput.attr('type') == 'password') {
+        passInput.attr('type', 'text');
+    } else {
+        passInput.attr('type', 'password');
+    }
+}
