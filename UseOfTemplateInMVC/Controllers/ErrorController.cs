cbd247a2771357d6059542452c766b16f450966f
Dharvi <tbs.dharvi@gmail.com>
2021-11-16 12:41:48 +0530

    Add 404 error page.

---
+++ UseOfTemplateInMVC/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UseOfTemplateInMVC.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult NotFound()
+        {
+            return PartialView("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
